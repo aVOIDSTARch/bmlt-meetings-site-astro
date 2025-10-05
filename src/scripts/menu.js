2eeeb97b2bbf259d3143ce4a8273a5f8bc268a8b
louisc <lcasine@my.wgu.edu>
2025-10-05 17:00:52 -0400

    tags lessons

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,7 +1,13 @@
 const menu = document.querySelector(".menu");
+const navLinks = document.querySelector(".nav-links");
 
 menu.addEventListener("click", () => {
     console.log("Menu clicked");
   const isExpanded = menu.getAttribute("aria-expanded") === "true";
   menu.setAttribute("aria-expanded", !isExpanded);
+  if (!isExpanded) {
+    navLinks.style.display = "block";
+  } else {
+    navLinks.style.display = "none";
+  }
 });
